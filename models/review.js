5f864ce2a8c5aced462ab2c492b3e92c5dd85507
Delta Student <Sambuddhaghosh8@gmail.com>
2025-07-30 02:54:30 +0530

    Initial commit

---
+++ models/review.js
@@ -0,0 +1,17 @@
+ const mongoose=require("mongoose");
+const { RegulationPage } = require("twilio/lib/rest/numbers/v2/regulatoryCompliance/regulation");
+ const Schema=mongoose.Schema;
+
+ const reviewSchema=new Schema({
+    comment: String,
+    rating:{
+        type:Number,
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+    },
+ });
+ module.exports = mongoose.model("Review", reviewSchema);
